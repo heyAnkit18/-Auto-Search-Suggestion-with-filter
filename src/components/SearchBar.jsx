0be79ac86f613dfd78188e1472d451c02bdbb2b7
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-11-03 22:02:42 +0530

    Search.jsx Updated

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,50 +6,48 @@ import Data from "../constants/states.json";
 const SearchBar = () => {
   const [searchInput, setSearchInput] = useState("");
   const [searchedData, setSearchedData] = useState([]);
+
   const handleChange = (e) => {
-    const value = e.target?.value;
+    const value = e.target.value;
     setSearchInput(value);
     if (value) {
-      const filteredData = Data.filter((items) => {
-        return items?.name
-          ?.toLocaleLowerCase()
-          .includes(value?.toLocaleLowerCase());
-      });
+      const filteredData = Data.filter((items) =>
+        items.name.toLowerCase().includes(value.toLowerCase())
+      );
       setSearchedData(filteredData);
     } else {
       setSearchedData([]);
     }
   };
+
   const handleClick = (name) => {
-    if (name) {
-      setSearchInput(name);
-      setSearchedData([]);
-    }
+    setSearchInput(name);
+    setSearchedData([]);
   };
+
   return (
     <div className="search-container">
+      <h2 className="search-heading">Search for Indian States</h2>
       <div className="search-bar">
         <input
           value={searchInput}
           onChange={handleChange}
-          placeholder="Search Heare..."
+          placeholder="Type to search..."
         />
         <div className="search-icon">
-          <img src={SearchIcon} width={25} />
+          <img src={SearchIcon} width={25} alt="Search Icon" />
         </div>
       </div>
-      {searchedData?.length ? (
+      {searchedData.length > 0 && (
         <div className="search-items">
           <ul>
-            {searchedData?.slice(0, 10).map((item) => {
-              return (
-                <li onClick={() => handleClick(item.name)}>{item?.name}</li>
-              );
-            })}
+            {searchedData.slice(0, 10).map((item) => (
+              <li key={item.key} onClick={() => handleClick(item.name)}>
+                {item.name}
+              </li>
+            ))}
           </ul>
         </div>
-      ) : (
-        ""
       )}
     </div>
   );
